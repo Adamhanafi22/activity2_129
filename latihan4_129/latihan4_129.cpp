6eb228929bf943de156eff84935367515118b7aa
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-03-20 15:46:53 +0700

    latihan4

--- latihan4_129/latihan4_129.cpp
+++ latihan4_129/latihan4_129.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using  namespace std;
 
-float luaspersegi(float p, float l); {
+float luaspersegi(float p, float l){
     return p * l;
 }
 float luaslingkaran(float r) {
     return 3.14 * r * r;
-  }
-float luassegitiga(float a, fliat t) {
+}
+float luassegitiga(float a, float t){
     return 0.5 * a * t;
 }
 
@@ -23,7 +23,34 @@ int main()
         cout << "1. luas persegi" << endl;
         cout << "2. luas lingkaran" << endl;
         cout << "3. luas segitiga" << endl;
+        cout << "4. exit" << endl;
+        cin >> pilihan;
 
+        switch (pilihan)
+        {
+        case 1:
+                cout << "masukan panjang = ";
+                cin >> panjang;
+                cout << "masukan lebar = ";
+                cin >> lebar;
+                cout << "luas persegi panjang = " << luaspersegi(panjang, lebar);
+                break;
+        case 2:
+            cout << "masykan jari - jari = ";
+            cin >> jejari;
+            cout << "luas lingkaran = " << luaslingkaran(jejari);
+            break;
+        case 3:
+            cout << "Masukan Alas = ";
+            cin >> alas;
+            cout << "masukan lebar = ";
+            cin >> tinggi;
+            cout << "luas segitiga = " << luassegitiga(alas, tinggi);
+            break;
+        case 4:
+            break;
 
-    }
+        }
+        
+    } while (pilihan != 4);
 }
